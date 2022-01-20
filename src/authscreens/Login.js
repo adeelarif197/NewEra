24e828b9b6ee801209ed72fbd01c76f9c393eef6
Mudassir Sheikh <mudassirsh17@yahoo.com>
2022-01-20 19:59:47 +0500

    Splash-Done

--- src/authscreens/Login.js
+++ src/authscreens/Login.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View ,StyleSheet,SafeAreaView,TextInput,Image} from 'react-native'
 
 export default class Login extends Component {
 	render() {
 		return (
-			<View>
-				<Text> LOGIN </Text>
+			<SafeAreaView>
+			<View style ={styles.LogoStyle}> 
+			<Image source={require('/Users/mudassirsheikh/Desktop/NewEra/NewEra/src/assests/NEW-ERA.png')}
+        />
 			</View>
+			<View style={styles.MainView}>
+			<TextInput style={styles.input}/>
+			<TextInput style={styles.input}/>
+			</View>
+				<Text> LOGIN </Text>
+			</SafeAreaView>
 		)
 	}
 }
+const styles = StyleSheet.create({
+	input:{
+		borderRadius:2,
+		borderWidth: 1,
+		
+		
+		
+		
+	},
+	MainView:{
+		// marginTop:'70%',
+		// marginHorizontal:'10%'
+
+
+	},
+	LogoStyle:{
+		// alignItems:'center',
+		height:10,
+		width:10
+
+	}
+
+})
